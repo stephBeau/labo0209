8e6dc12abe9bc896b6c6dcf4d67dd5db618aa0a3
stephbeau <stephane.beaulieu@rocketmail.com>
2017-02-09 20:59:51 -0500

    Version1

--- src/labo0209/Labo0209.java
+++ src/labo0209/Labo0209.java
@@ -15,7 +15,7 @@ public class Labo0209 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Version 1
     }
     
 }
